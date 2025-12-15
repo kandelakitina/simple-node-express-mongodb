10fae0aa71a78b8b3515836875fc6f6d457ba918
boticelli <kandelakitina@gmail.com>
2025-12-15 12:09:03 +0300

    Add db config

---
+++ config/db.js
@@ -0,0 +1,23 @@
+// blog_app/config/db.js
+
+import mongoose from "mongoose";
+
+export default function connectDB() {
+	const url = "mongodb://localhost:27017";
+
+	try {
+		mongoose.connect(url);
+	} catch (err) {
+		console.error(err.message);
+		process.exit(1);
+	}
+	const dbConnection = mongoose.connection;
+	dbConnection.once("open", (_) => {
+		console.log(`Database connected: ${url}`);
+	});
+
+	dbConnection.on("error", (err) => {
+		console.error(`connection error: ${err}`);
+	});
+	return;
+}
